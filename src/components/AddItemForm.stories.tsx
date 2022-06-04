eaf75a4f6916b5c1fc5070ee744aee004a5d10cc
3admitry <3a.dmitriy@gmail.com>
2022-06-04 03:44:00 +0300

    created StoryBook, add AddItemForm.stories.tsx

---
+++ src/components/AddItemForm.stories.tsx
@@ -0,0 +1,32 @@
+// Button.stories.ts|tsx
+
+import React from 'react';
+
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import {AddItemForm} from './AddItemForm';
+
+export default {
+    title: 'AddItemForm',
+    component: AddItemForm,
+    argTypes: {
+        addItem: { action: 'User add new item:' }
+    },
+} as ComponentMeta<typeof AddItemForm>;
+
+/*export const BaseExample: ComponentStory<typeof AddItemForm> = (argTypes) => {
+    return (
+        <>
+            <AddItemForm addItem={argTypes.addItem} />
+        </>
+    )
+}*/
+
+//👇 We create a “template” of how args map to rendering
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+
+export const BaseExample = Template.bind({});
+
+BaseExample.args = {};
+
